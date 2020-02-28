966f9fa171471990265b5487c93a24309e08a408
djosifovic <dejan.josifovic@igt.com>
2020-02-28 18:17:21 +0100

    Add more state to tasks in org.

--- init.el
+++ init.el
@@ -251,6 +251,7 @@
   :bind (("C-c a" . 'org-agenda))
   :config
   (setq org-log-done t)
+  (setq org-todo-keywords '((sequence "TODO(t)" "HOLD(h!)" "|" "DONE(d@!)" "CANCELED(c@!)")))
   (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
   (add-to-list 'org-src-lang-modes '("dot" . graphviz-dot-mode)))
 
