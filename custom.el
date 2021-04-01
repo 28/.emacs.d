385b7cc9bc6f327d810c96f127af46d8ced4927a
Dejan Josifović <dejan.josifovic@igt.com>
2021-04-01 13:52:46 +0200

    Update custom file

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons-dired all-the-icons elpher elfeed plantuml-mode magit paredit zenburn-theme use-package)))
+   '(gemini-mode all-the-icons-dired all-the-icons elpher elfeed plantuml-mode magit paredit zenburn-theme use-package)))
 
 ;; custom-set-faces was added by Custom.
 ;; If you edit it by hand, you could mess it up, so be careful.
