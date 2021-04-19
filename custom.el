0729a1ddf1467f6bfa800a9e692438ef3cd1f99e
Dejan Josifović <dejan.josifovic@igt.com>
2021-04-19 15:42:36 +0200

    Remodel custom.el

--- custom.el
+++ custom.el
@@ -1,22 +1,15 @@
-;; custom-set-variables was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; org-mode
+ '(org-directory "./") ; change me
+ '(org-agenda-files (list org-directory))
+
+ ;; plantuml-mode
+ '(plantuml-jar-path "./plantuml.jar") ; change me
+
+ ;; elfeed
+ '(elfeed-feeds (quote ("https://theparanoidtimes.org/feed.xml")))
+
  '(package-selected-packages
    '(gemini-mode all-the-icons-dired all-the-icons elpher elfeed plantuml-mode magit paredit zenburn-theme use-package)))
 
-;; custom-set-faces was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
